63e5fd254bd00b469131f16d80779dbdca9bf8fe
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2019-12-03 13:09:58 +0100

    fix tests

--- src/test/scala/nl/getthere/dataunit/iolink/DistanceTest.scala
+++ src/test/scala/nl/getthere/dataunit/iolink/DistanceTest.scala
@@ -1,4 +1,4 @@
-package nl.getthere.dataunit
+package nl.getthere.dataunit.iolink
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -8,13 +8,13 @@ class DistanceTest {
   def testDistance29(): Unit = {
     val hex = "01D1"
     val expected = 29
-    assertEquals(expected, IoLinkReader.distance(hex))
+    assertEquals(expected, Distance.extract(hex))
   }
 
   @Test
   def testDistanceIntOverflow(): Unit = {
     val hex = "0411"
     val expected = 65
-    assertEquals(expected, IoLinkReader.distance(hex))
+    assertEquals(expected, Distance.extract(hex))
   }
 }
